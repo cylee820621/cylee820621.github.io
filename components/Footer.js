7216ab2736b19277840631960a922c21a6cd7bfb
CYLee <cylee820621@gmail.com>
2022-01-05 20:51:35 -0500

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,26 +1,31 @@
+import Image from 'next/image';
 const Footer = () => {
   return (
-    <footer className='flex bg-black text-white justify-center items-center h-40 mt-10'>
-      <div className='flex flex-col '>
-        <div className='flex justify-center items-center'>
-          <div>
-            <a href='https://www.linkedin.com/in/chih-yu-lee/'>
-              <img
-                width='50px'
-                src='https://img.icons8.com/color/48/000000/linkedin.png'
-              />
-            </a>
-          </div>
-          <dir>
-            <a href='https://github.com/cylee820621'>
-              <img
-                width='50px'
-                src='https://img.icons8.com/nolan/64/github.png'
-              />
-            </a>
-          </dir>
+    <footer className='flex flex-col bg-black justify-center items-center h-40 mt-10'>
+      <div className='flex justify-center items-center'>
+        <div>
+          <a href='https://www.linkedin.com/in/chih-yu-lee/'>
+            <Image
+              width={50}
+              height={50}
+              alt='linkedin'
+              src='https://img.icons8.com/color/48/000000/linkedin.png'
+            />
+          </a>
         </div>
-        <div>Copyright - 2021 Chih Yu Lee, All Right Reserved</div>
+        <dir>
+          <a href='https://github.com/cylee820621'>
+            <Image
+              width={50}
+              height={50}
+              alt='github'
+              src='https://img.icons8.com/nolan/64/github.png'
+            />
+          </a>
+        </dir>
+      </div>
+      <div className='text-white '>
+        Copyright - 2021 Chih Yu Lee, All Right Reserved
       </div>
     </footer>
   );
--- components/Stack.js
+++ components/Stack.js
@@ -3,7 +3,13 @@ import Image from 'next/image';
 const Stack = ({ imageUrl, name }) => {
   return (
     <div className='flex flex-col items-center m-5'>
-      <Image className='w-30' src={imageUrl} width={80} height={80} />
+      <Image
+        className='w-30'
+        src={imageUrl}
+        alt={name}
+        width={80}
+        height={80}
+      />
       <h4>{name}</h4>
     </div>
   );
